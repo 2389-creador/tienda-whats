b2f14004d5c401545e855e6f65d4a625897cd3da
izhostMX <interzonegdl2@gmail.com>
2022-01-07 02:47:52 -0600

    coreTW2389

---
+++ admin/plugins.php
@@ -0,0 +1,39 @@
+<?php
+require_once('includes.php');
+//$server_file_path = "https://bylancer.com/api/quickjob-advertise.json";
+//$ads = json_decode(file_get_contents($server_file_path),true);
+?>
+<style>
+    .pro-list-img{
+        min-height: 150px;
+        background: url("assets/images/quickad_loading.gif") no-repeat center;
+    }
+</style>
+<main class="app-layout-content">
+
+    <!-- Page Content -->
+    <div class="container-fluid p-y-md">
+        <!-- Partial Table -->
+        <div class="card">
+            <div class="card-header">
+                <h4>Plugins</h4>
+            </div>
+            <div class="card-block">
+                <!-- /row -->
+            </div>
+            <!-- .card-block -->
+        </div>
+        <!-- .card -->
+        <!-- End Partial Table -->
+
+    </div>
+    <!-- .container-fluid -->
+    <!-- End Page Content -->
+
+</main>
+
+
+<?php include('footer.php'); ?>
+</body>
+
+</html>
